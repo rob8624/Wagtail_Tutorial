76bb3f7d69689d77fd511d8798498b1bb3f7307f
Rob8624 <robert_melen@yahoo.co.uk>
2022-03-10 09:35:43 +0000

    Initial

---
+++ blog/blog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-h2m^+7ls)q2k&ljkb6a1v^w&=9d+_^rb*m@*=6c&itk5%e*0xq'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
